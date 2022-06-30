1ad80c1d2bc3004b3b450b2d4fbcd183d42c4352
Akshayteli-111 <93920968+Akshayteli-111@users.noreply.github.com>
2022-06-30 21:57:16 +0530

    Update fetchPosts.js

--- src/api/fetchPosts.js
+++ src/api/fetchPosts.js
@@ -1,5 +1,8 @@
 const fetchPosts = async (page, limit) => {
     // optionally write fetching logic here or somewhere else
-}
+    let url='https://jsonplacehoder.typicode.com/posts?_page=${page}&_limit}';
+    return fetch(url);
+    
+};
 
-export { fetchPosts }
+export { fetchPosts };
