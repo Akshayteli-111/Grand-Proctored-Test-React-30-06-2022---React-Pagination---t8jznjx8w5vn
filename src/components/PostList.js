1e1c33e07b69f1583a0ed43c55e866cd9a35fa1f
Akshayteli-111 <93920968+Akshayteli-111@users.noreply.github.com>
2022-06-30 22:32:39 +0530

    Update PostList.js

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,13 +1,44 @@
-import React from 'react'
-
-
-const PostList = () => {
+import React ,{useEffect, useState } from "react";
+import { fetchPosts } from "../api/fetchPosts";
+import {PaginationButtonsList} from "./PaginationButtonsLIst";
+import {Post} from "./Post";
+const PostList =()=>{
+    const [data,setData]= useState(null);
+    const [page,setPage]=useState(1);
     
-    return (
+    const lidadData = async ()=>{
+        fetchPosts(page,5)
+        .then((res)=>res.json())
+        .then((jsonData) => {
+            setData(jsonData);
+        });
+    };
+    useEffect(() =>
+              lisdData();
+              }, []);
+      useEffect(() =>
+                setData(null);
+              lisdData();
+              }, [page]);
+
+      const clickHandler = (val) => {
+          setPage(val);
+      };
+
+       return (
         <>
-
+        {data ==null ? (
+         <div id="loader" className="loader">
+         loading
+         </div>
+         ):(
+        data.map((ele) =>{
+            return <Post ele={ele} key={ele.id}/>;
+        })
+        )}
+          <paginationButtonsList page={page} clickHandler={clickHandler}/>
         </>
-    )
-}
+    );
+};
 
-export { PostList }
+export { PostList };
